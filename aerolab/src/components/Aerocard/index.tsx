fd1cc6ad5f2b5df508bac0a46ab44dc85ae33959
matipeco <matiaspecorale1995@gmail.com>
2023-05-01 19:55:25 -0300

    terminando componente aerocard

--- aerolab/src/components/Aerocard/index.tsx
+++ aerolab/src/components/Aerocard/index.tsx
@@ -1,5 +1,22 @@
 import { StyledAerocard } from "./style";
+import Image from "next/image";
 
 export const Aerocard = () => {
-  return <StyledAerocard>aerocard</StyledAerocard>;
+  return (
+    <StyledAerocard>
+      <div>
+        <p className="aerocard-title">Aerocard</p>
+        <Image
+          src="/assets/icons/aeropay-2.svg"
+          alt=""
+          width="24"
+          height="24"
+        />
+      </div>
+      <div>
+        <p>Matias Pecorale</p>
+        <p>07/23</p>
+      </div>
+    </StyledAerocard>
+  );
 };
--- aerolab/src/components/Aerocard/style.ts
+++ aerolab/src/components/Aerocard/style.ts
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
 export const StyledAerocard = styled.div`
-  background-color: red;
-`
+  background-color: var(--neutral-900);
+  border-radius: 0.8rem;
+  padding: 1.6rem;
+  color: var(--neutral-100);
+  height: 14.8rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  position: relative;
+  overflow: hidden;
+  margin-bottom: 4rem;
+  font-size: 1.4rem;
+  &::after {
+    content: "";
+    display: block;
+    background-image: url("/assets/illustrations/single-wave-pattern.svg");
+    opacity: 0.2;
+    position: absolute;
+    inset: 50% -1rem -3rem;
+    transform: rotate(-10deg);
+  }
+
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .aerocard-title {
+    font-size: 1.8rem;
+    font-weight: 600;
+  }
+`;
