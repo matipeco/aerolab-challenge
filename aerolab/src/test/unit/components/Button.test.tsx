fcf6179969c7a59b1863906a37fa9a92e2bcf517
matipeco <matiaspecorale1995@gmail.com>
2023-05-03 20:51:59 -0300

    test para componente Button segun la clase que recibe

--- aerolab/src/test/unit/components/Button.test.tsx
+++ aerolab/src/test/unit/components/Button.test.tsx
@@ -2,9 +2,27 @@ import { render } from "@testing-library/react";
 import { Button } from "@/components/Button";
 
 describe("Componente Button", () => {
-  test("se renderiza correctamente", () => {
+  test("Se renderiza correctamente", () => {
+    const { container } = render(<Button className="clase">boton</Button>);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("Agrega la clase pending", () => {
     const { container } = render(
-      <Button className="clase">soy el boton</Button>
+      <Button className="classname" isPending>
+        boton
+      </Button>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("Agrega la clase disabled", () => {
+    const { container } = render(
+      <Button className="classname" disabled>
+        boton
+      </Button>
     );
 
     expect(container).toMatchSnapshot();
